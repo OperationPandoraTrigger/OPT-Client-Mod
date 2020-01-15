8d22909fd70f024e385d58f559d234fe47b3b15d
GNCLord-MDB <lord.mdb@gmx.net>
2020-01-15 17:14:24 +0100

    OPT NATO Prowler AT Tanoa

--- addons/opt_vehicles/unarmed/config.cpp
+++ addons/opt_vehicles/unarmed/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches
 				   "OPT4_B_APC_Wheeled_01_cannon_F", "OPT4_B_APC_Wheeled_01_cannon_F", "OPT4_B_APC_Tracked_01_rcws_F", "OPT4_B_APC_Tracked_01_AA_F", "OPT4_B_MBT_01_cannon_F",
 				   "OPT4_B_MBT_01_TUSK_F", "OPT4_B_MBT_01_arty_F", "OPT4_B_MBT_01_mlrs_F", "OPT4_O_APC_Wheeled_02_rcws_F", "OPT4_O_T_APC_Wheeled_02_rcws_ghex_F", "OPT4_B_APC_tracked_03_cannon_F", "OPT4_O_APC_Tracked_02_cannon_F",
 				   "OPT4_O_T_APC_Tracked_02_cannon_ghex_F", "OPT4_O_APC_Tracked_02_AA_F", "OPT4_O_T_APC_Tracked_02_AA_ghex_F", "OPT4_O_MBT_02_cannon_F", "OPT4_O_T_MBT_02_cannon_ghex_F", "OPT4_O_MBT_02_arty_F", "OPT4_O_T_MBT_02_arty_ghex_F",
-				   "OPT4_O_APC_Wheeled_03_cannon_F","OPT4_O_APC_Wheeled_03_cannon_ghex_F", "OPT4_B_MBT_03_cannon_F", "OPT4_B_LSV_01_AT_F", "OPT4_O_LSV_02_AT_F", "OPT4_O_Truck_02_MRL_F","OPT4_O_LSV_02_AT_GHEX_F"};
+				   "OPT4_O_APC_Wheeled_03_cannon_F","OPT4_O_APC_Wheeled_03_cannon_ghex_F", "OPT4_B_MBT_03_cannon_F", "OPT4_B_LSV_01_AT_F","OPT4_B_LSV_01_AT_ghex_F", "OPT4_O_LSV_02_AT_F", "OPT4_O_Truck_02_MRL_F","OPT4_O_LSV_02_AT_GHEX_F"};
 		weapons[] = {};
 		requiredVersion = 0.100000;
 		requiredAddons[] = {"opt_weapons", "opt_characters", "opt_core", "a3_armor_f", "a3_soft_f", "a3_soft_f_mrap_01", "a3_soft_f_mrap_02", "a3_soft_f_mrap_03", "a3_armor_f_panther",
@@ -1007,6 +1007,54 @@ class CfgVehicles
 		displayName = "Prowler (AT)";
 		fuelCapacity = 3; // 20 //
 		maximumLoad = 2500;
+		textureList[] = {"Black",0,"Olive",0,"Sand",1};
+
+		class TransportMagazines
+		{
+		};
+
+		class TransportItems
+		{
+		};
+
+		class TransportWeapons
+		{
+		};
+
+		class Turrets : Turrets
+		{
+			class CargoTurret_02 : CargoTurret_02
+			{
+			};
+
+			class CargoTurret_03 : CargoTurret_03
+			{
+			};
+
+			class CodRiverTurret : CodRiverTurret
+			{
+			};
+
+			class TopTurret : TopTurret
+			{
+				magazines[] = {};
+				weapons[] = {};
+
+				class ViewOptics : ViewOptics
+				{
+					visionMode[] = {"Normal", "NVG"};
+				};
+			};
+		};
+	};
+
+	class OPT4_B_LSV_01_AT_ghex_F : B_LSV_01_AT_F
+	{
+		faction = "OPT_NATO_T";
+		displayName = "Prowler (AT)";
+		fuelCapacity = 3; // 20 //
+		maximumLoad = 2500;
+		textureList[] = {"Black",0,"Olive",1,"Sand",0};
 
 		class TransportMagazines
 		{
