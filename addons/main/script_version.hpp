6e807fb6b83120c472426f882ab3727ac32c9908
formtapez <github.com@posthub.de>
2021-04-02 17:20:20 +0200

    added forgotton folder

---
+++ addons/main/script_version.hpp
@@ -0,0 +1,15 @@
+// Define version information of this mod
+#define MAJOR 3
+#define MINOR 5
+#define PATCHLVL 0
+#define BUILD 0
+
+#ifdef VERSION
+    #undef VERSION
+#endif
+#ifdef VERSION_AR
+    #undef VERSION_AR
+#endif
+
+#define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
+#define VERSION MAJOR.MINOR.PATCHLVL.BUILD
