600b7aad94b738d0c598df21cd8e02ca9064e1ff
GNCLORD-MDB <39137673+GNCLORD-MDB@users.noreply.github.com>
2021-05-26 17:34:31 +0200

    fix

--- addons/vehicles/apc/config.cpp
+++ addons/vehicles/apc/config.cpp
@@ -3006,7 +3006,7 @@ class CfgVehicles
                 weapons[] = {"CUP_Vacannon_2A42_veh", "CUP_Vhmg_PKT_veh_Noeject", "CUP_Vmlauncher_AT5_single_veh"};
                 magazines[] = {"CUP_250Rnd_TE1_Green_Tracer_30mmHE_2A42_M", "CUP_250Rnd_TE1_Green_Tracer_30mmAP_2A42_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_250Rnd_TE1_Green_Tracer_762x54_PKT_M", "CUP_8Rnd_AT5_BMP2_M", "CUP_400Rnd_30mm_AGS17_M"};
             };
-		};
+        };
 
         class TransportMagazines
         {
@@ -3052,7 +3052,7 @@ class CfgVehicles
                 weapons[] = {"OPT_CUP_Vacannon_M168_M163VADS", "SmokeLauncher"};
                 magazines[] = {"OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M","OPT_CUP_50Rnd_TE1_Red_Tracer_20mm_M168_M", "SmokeLauncherMag"};
             };
-		};
+        };
 
         class TransportMagazines
         {
@@ -3097,7 +3097,7 @@ class CfgVehicles
                 weapons[] = {"OPT_CUP_Vacannon_AZP23_veh", "SmokeLauncher"};
                 magazines[] = {"OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M","OPT_CUP_50Rnd_23mm_AZP23_M", "SmokeLauncherMag"};
             };
-		};
+        };
 
         class TransportMagazines
         {
